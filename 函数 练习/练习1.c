5e81926eecb7a7ca5552b7a61f2d6d4b337cd47c
zht88821 <83168270+zht88821@users.noreply.github.com>
2021-04-30 21:14:08 +0800

    函数

---
+++ 函数 练习/练习1.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<math.h>
+
+//int get_max(int x, int y)
+//{
+//	return (x > y) ? (x) : (y);
+// }
+//
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int max = get_max(num1, num2);
+//	printf("max = %d\n", max);
+//	int max1 = get_max(100, 300);
+//	printf("max1 = %d\n", max1);
+//	return 0;
+//}
+
+//void Swap1(int x, int y)//不能完成交换
+//{//Swap1---传值调用
+//	int tmp = 0;
+//	tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+//
+//void Swap2(int* pa, int* pb)//能完成交换
+//{//Swap2---传址调用
+//	int tmp = 0;
+//	tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//
+//int main() //交换函数的实现。
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a = %d , b = %d \n", a, b);
+//	Swap1(a, b);
+//	printf("a = %d , b = %d \n ",a,b);
+//	Swap2(&a, &b);
+//	printf("a = %d , b = %d \n", a, b);
+//
+//	return 0;
+//}
+
+//是素数返回1，不是素数返回0
+int is_prime(int n)   //实现判断是否为素数的函数
+{
+	int m = 0;
+	for (m = 2; m < sqrt(n); m++)
+	{
+		if (n % m == 0)
+		{
+			break;
+		}
+			
+	}
+	if (m > sqrt(n))7
+		return 1;
+	else
+		return 0;
+}
+
+int main()
+{
+	//打印100-200之间的素数
+	int i = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		//判断i是否为素数
+		if (is_prime(i) == 1)
+			printf("%d\n", i);
+	}
+	return 0;
+
+}
