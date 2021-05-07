3f41667bc9e203ba436d6b9176b5b9210f75470e
zht88821 <83168270+zht88821@users.noreply.github.com>
2021-05-07 22:23:25 +0800

    1

--- 汉诺塔游戏代码/test.c
+++ 汉诺塔游戏代码/test.c
@@ -8,15 +8,15 @@ void move(char a, char b)
 
 void hannuota(int n, char a, char b, char c)//表示有n个盘子，从a利用b转移到c.
 {
-	if (n == 1)
+	if (n == 1)//递归函数的出口
 	{
 		printf("%c-->%c\n",a,c);                     //汉诺塔游戏大致分为三个步骤（a,b,c三根柱子，由a利用b转移到c的情况）
 	}                                                //①先将a柱子上的n-1个盘子利用c转移到b。
 	else                                             //②再将a柱子上最后一个大盘子转移到c。
 	{                                                //③再将b柱子上的n-1个盘子利用a转移到c。
-		hannuota(n - 1, a, c, b);//递归调用          //第①步和第③步可以细分重复这三个步骤，实现了递归调用。
-		move(a, c);
-		hannuota(n - 1, b, a, c);
+		hannuota(n - 1, a, c, b);//第①步            //第①步和第③步可以细分重复这三个步骤，实现了递归调用。
+		move(a, c);              //第②步
+		hannuota(n - 1, b, a, c);//第③步
 	}
 
 }
