f3c1bc4d0846aca7e3da5399941e14374b1749ad
zht88821 <83168270+zht88821@users.noreply.github.com>
2021-05-10 23:54:27 +0800

    1

---
+++ 三子棋游戏/game.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<stdio.h>
+#define ROW 3
+#define COL 3
+
+
+//º¯ÊıµÄÉùÃ÷
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
