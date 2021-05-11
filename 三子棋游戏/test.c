fb9cee35754d0a3ed1a007663dab59d0bcac8f23
zht88821 <83168270+zht88821@users.noreply.github.com>
2021-05-11 17:38:53 +0800

    三子棋完结

--- 三子棋游戏/test.c
+++ 三子棋游戏/test.c
@@ -12,16 +12,52 @@ void menu()
 //游戏的整个实现
 void game()
 {
+	char ret = 0;
 	//数组-存放走出的棋盘信息
 	char board[ROW][COL] = { 0 };//初始棋盘全为空格
 	InitBoard(board,ROW,COL);//初始化棋盘
 	//打印棋盘
 	DisplayBoard(board,ROW,COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+		//判断玩家是否赢
+		ret = IsWin(board,ROW,COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//电脑下棋
+		ComputerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+		//判断电脑是否赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
 }
 
 void test()
 {
 	int input = 0;
+	srand((unsigned int)time(NULL));
 	do
 	{
 		menu();
