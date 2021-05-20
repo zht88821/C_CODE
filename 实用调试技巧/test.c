96201835f3541f89e7bc8b2a3fb00f68c41a0521
zht88821 <83168270+zht88821@users.noreply.github.com>
2021-05-20 18:41:58 +0800

    1

---
+++ 实用调试技巧/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", 10-i);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;//保存最终结果
+//	int n = 0;
+//	int ret = 1;//保存n的阶乘
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		ret = 1;//通过调试找到bug,少了每次算一个数的阶乘给ret赋值1
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (i = 0; i <= 12; i++)
+	{
+		printf("hehe\n");
+		arr[i] = 0;           //通过调试发现死循环的原因是越界的arr[12]与
+	}                         //i用的是同一块空间，把arr[12]赋值成0的同时
+	system("pause");         //将i也赋值成0，造成了死循环
+
+	return 0;
+}
