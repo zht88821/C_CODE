e92bbc126e80a329966e547daa2298b35b47d9aa
zht88821 <83168270+zht88821@users.noreply.github.com>
2021-05-31 18:27:48 +0800

    作业

--- 作业练习/test.c
+++ 作业练习/test.c
@@ -301,3 +301,191 @@
 
 
 
+////1.暴力求解法
+//void left_move(char* arr, int k)//左旋转字符串函数,k代表旋转的个数  比如abcdef左旋转一个变成bcdefa
+//{
+//	assert(arr != NULL);
+//	int i = 0;
+//	int len = strlen(arr);
+//	for (i = 0; i < k; i++)
+//	{
+//		//1
+//		char tmp = *arr;
+//		//2
+//		int j = 0;
+//		for (j = 0; j < len - 1; j++)
+//		{
+//			*(arr + j) = *(arr + j + 1);
+//		}
+//		//3
+//		*(arr + len - 1) = tmp;
+//
+//	}
+//}
+
+
+////2.三步翻转法
+//// 假设左旋两个
+////ab cdef
+////ba fedc   把ab  cdef各自旋转
+////cdefab    再将整个字符串旋转
+////
+//
+////逆序字符串的函数
+//void reverse(char* left, char* right)
+//{
+//	assert(left != NULL);
+//	assert(right != NULL);
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//
+//void left_move(char* arr, int k)//三步翻转法
+//{
+//	assert(arr);
+//	int len = strlen(arr);
+//	assert(k <= len);
+//
+//	reverse(arr, arr + k - 1);//逆序左边
+//	reverse(arr + k, arr + len - 1);//逆序右边
+//	reverse(arr, arr + len - 1);//逆序整体
+//}
+//
+//
+////判断一个字符串是否为另外一个字符串旋转的结果的判定函数
+//int is_left_move(char* s1, char* s2)
+//{
+//	int len = strlen(s1);
+//	int i = 0;
+//	for (i = 0; i < len; i++)
+//	{
+//		left_move(s1, 1);
+//		int ret = strcmp(s1, s2);
+//		if (ret == 0)
+//			return 1;
+//	}
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "cdefab";
+//	int ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//		printf("Yes\n");
+//	else
+//		printf("No\n");
+//
+//	return 0;
+//}
+
+
+
+
+//int is_left_move(char* str1, char* str2)
+//{
+//	int len1 = strlen(str1);
+//	int len2 = strlen(str2);
+//	if (len1 != len2)
+//		return 0;
+//
+//	//1.在str1字符串中追加一个str1字符串
+//	//strcat(str1,str1);//err  strcat不能用来自己给自己追加字符串
+//	strncat(str1, str1, len1);
+//	//2.判断str2指向的字符串是否是str1指向的字符串的子串
+//	//strstr - 找子串的 返回子串的首元素地址
+//	char* ret = strstr(str1, str2);
+//	if (ret == NULL)
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		return 1;
+//	}
+//}
+//
+//
+//int main()
+//{
+//	char arr1[30] = "abcdef";
+//	char arr2[] = "cdefab";
+//	int  ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//	{
+//		printf("Yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//
+//	return 0;
+//}
+
+
+
+//题目名称：
+//杨氏矩阵
+//题目内容：
+//有一个数字矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的，
+//请编写程序在这样的矩阵中查找某个数字是否存在。
+//要求：时间复杂度小于O(N);
+
+int FindNum(int arr[3][3], int k, int* px, int* py)
+{
+	int x = 0;
+	int y = *py - 1;
+
+	while (x <= *px - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
+
+	}
+	//找不到
+	return 0;
+}
+
+int main()
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 9;
+	int x = 3;
+	int y = 3;
+
+	//返回型参数
+	int ret = FindNum(arr, k, &x, &y);
+	if (ret == 1)
+	{
+		printf("找到了\n");
+		printf("下标是:%d %d\n", x, y);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
+	
+	return 0;
+}
