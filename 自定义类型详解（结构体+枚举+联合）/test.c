3cc611abde5aef601df08c2def5147c9c7bef940
zht88821 <83168270+zht88821@users.noreply.github.com>
2021-06-07 10:31:47 +0800

    1

--- 自定义类型详解（结构体+枚举+联合）/test.c
+++ 自定义类型详解（结构体+枚举+联合）/test.c
@@ -291,3 +291,132 @@
 //
 //	return 0;
 //}
+
+
+//
+////枚举类型
+//enum Sex
+//{
+//	//枚举的可能取值 - 常量
+//	MALE = 2,//2
+//	FEMALE = 4,//4
+//	SECRET = 8//8
+//};
+//
+//
+//enum Color
+//{
+//	RED,//0
+//	GREEN,//1
+//	BLUE//2
+//};
+//
+//int main()
+//{
+//	enum Sex s = MALE;
+//	enum Color c = BLUE;
+//	//enum Sex ss = man;//err
+//	printf("%d %d %d\n", RED, GREEN, BLUE);
+//	printf("%d %d %d\n", MALE, FEMALE, SECRET);
+//
+//	return 0;
+//}
+
+
+
+
+//联合 - 联合体 - 共用体
+
+//union Un
+//{
+//	char c;//1
+//	int i;//4
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));//4
+//	printf("%p\n", &u);
+//	printf("%p\n", &u.c);
+//	printf("%p\n", &u.i);
+//
+//	return 0;
+//}
+
+
+//int check_sys()//用指针方式判断大小端字节序的函数
+//{
+//	int a = 1;
+//	//返回1 ，表示小端
+//	//返回0 ， 表示大端
+//	return *(char*)&a;
+//}
+//
+//int check_sys2()//用联合体方式判断大小端字节序的函数
+//{
+//	union Un
+//	{
+//		char c;
+//		int i;
+//	}u;
+//
+//	u.i = 1;
+//	//返回1，表示小端
+//	//返回0，表示大端
+//	return u.c;
+//}
+//
+//int main()
+//{
+//	int ret = check_sys2();
+//
+//	if (1 == ret)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//
+//	//int a = 0x11223344;
+//	//
+//	//低地址-------------------->高地址
+//	//....[][][][][11][22][33][44][][][][][][][]...  大端字节序存储模式
+//	//....[][][][][44][33][22][11][][][][][][][]...  小端字节序存储模式
+//	//讨论一个数据，放在内存中的存放的字节顺序
+//	//大小端字节序问题
+//	
+//	return 0;
+//}
+
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	enum Sex s = MALE;
+//	printf("%d\n", sizeof(s));//枚举变量的大小为一个整型大小
+//
+//	return 0;
+//}
+
+
+//union Un
+//{
+//	int a;//对齐数-4
+//	char arr[5];//对齐数- 1
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));//8
+//
+//	return 0;
+//}
